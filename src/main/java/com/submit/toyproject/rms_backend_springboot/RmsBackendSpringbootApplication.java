9064b958952745936dfb74951923beb1ee0e8307
이승윤 <yyuunn17@naver.com>
2021-08-26 15:12:03 +0900

    [INIT]

---
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/RmsBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.submit.toyproject.rms_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/submit/toyproject/rms_backend_springboot/RmsBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submit.toyproject.rms_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
