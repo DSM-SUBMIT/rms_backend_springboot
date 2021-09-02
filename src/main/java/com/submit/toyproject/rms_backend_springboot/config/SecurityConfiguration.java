a692bbd03013f0b47bb49a124faaa80564406b93
이승윤 <yyuunn17@naver.com>
2021-09-02 23:19:57 +0900

    [추가] security config

---
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.submit.toyproject.rms_backend_springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT");
+    }
+
+}
