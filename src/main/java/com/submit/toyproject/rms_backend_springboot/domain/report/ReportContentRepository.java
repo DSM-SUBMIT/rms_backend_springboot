163e7b9a41ef83d9f2f213d42a593158d22169e2
이승윤 <yyuunn17@naver.com>
2021-09-03 16:29:57 +0900

    [UPDATE] 보고서 콘텐트 칼럼으로 추가 / MongoDB 삭제

--- src/main/java/com/submit/toyproject/rms_backend_springboot/domain/report/ReportContentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.submit.toyproject.rms_backend_springboot.domain.report;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReportContentRepository extends MongoRepository<ReportContent, Integer> {
-}
