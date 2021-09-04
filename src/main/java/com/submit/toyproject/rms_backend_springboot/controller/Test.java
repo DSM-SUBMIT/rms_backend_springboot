7b86c9db1ce7abfee097cdab72672ecbe2c94cf4
이승윤 <yyuunn17@naver.com>
2021-09-04 21:50:44 +0900

    [DELETE] Test class

--- src/main/java/com/submit/toyproject/rms_backend_springboot/controller/Test.java
+++
@@ -1,4 +0,0 @@
-package com.submit.toyproject.rms_backend_springboot.controller;
-
-public class Test {
-}
--- src/main/java/com/submit/toyproject/rms_backend_springboot/service/Test.java
+++
@@ -1,4 +0,0 @@
-package com.submit.toyproject.rms_backend_springboot.service;
-
-public class Test {
-}
