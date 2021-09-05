6d84248c76749970ee0641229d4afe6cd998f6ac
이승윤 <yyuunn17@naver.com>
2021-09-05 19:49:07 +0900

    [UPDATE] dto

--- src/main/java/com/submit/toyproject/rms_backend_springboot/domain/language/LanguageRepository.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/domain/language/LanguageRepository.java
@@ -3,4 +3,5 @@ package com.submit.toyproject.rms_backend_springboot.domain.language;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LanguageRepository extends CrudRepository<Language, Integer> {
+    Boolean existsByLanguage(String language);
 }
