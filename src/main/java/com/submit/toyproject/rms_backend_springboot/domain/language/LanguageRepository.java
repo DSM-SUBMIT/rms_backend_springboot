be1525c30e89a4f462adbd1a944d8876b5ee3a39
이승윤 <yyuunn17@naver.com>
2021-09-05 21:29:05 +0900

    [ADD] JPA method

--- src/main/java/com/submit/toyproject/rms_backend_springboot/domain/language/LanguageRepository.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/domain/language/LanguageRepository.java
@@ -2,6 +2,9 @@ package com.submit.toyproject.rms_backend_springboot.domain.language;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LanguageRepository extends CrudRepository<Language, Integer> {
     Boolean existsByLanguage(String language);
+    List<Language> findByLanguageLike(String language);
 }
