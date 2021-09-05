c950690d0c361bc8a99d29b714a6b7f779f68592
이승윤 <yyuunn17@naver.com>
2021-09-05 17:07:58 +0900

    [FIX]

--- src/main/java/com/submit/toyproject/rms_backend_springboot/controller/TeamController.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/controller/TeamController.java
@@ -4,10 +4,9 @@ import com.submit.toyproject.rms_backend_springboot.dto.request.TeamRequest;
 import com.submit.toyproject.rms_backend_springboot.service.team.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/team")
@@ -18,7 +17,8 @@ public class TeamController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void saveTeam(TeamRequest request) {
+    public void saveTeam(@RequestBody @Valid TeamRequest request) {
+        System.out.println(request.getName());
         teamService.saveTeam(request.getName());
     }
 
