87dd9f0e353649e5f96627ea7c13af51fd264d6c
kwakdony <kwakdh25@gmail.com>
2021-09-07 00:03:10 +0900

    [UPDATE] dao

--- src/main/java/com/submit/toyproject/rms_backend_springboot/domain/user/UserRepository.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/domain/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.submit.toyproject.rms_backend_springboot.domain.user;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     List<User> findByNameLike(String name);
+    Optional<User> findByEmail(String email);
 }
