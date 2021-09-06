8ad994b9caddbc865d61f0b451769d3ea0817245
kwakdony <kwakdh25@gmail.com>
2021-09-06 21:43:18 +0900

    [ADD] dto

---
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.submit.toyproject.rms_backend_springboot.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+}
