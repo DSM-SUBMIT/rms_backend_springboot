75a23af7c2908b8d517290b53fb97972f3e2b529
kwakdony <kwakdh25@gmail.com>
2021-09-09 13:33:37 +0900

    [ADD] AuthDetails, AuthDetailService

---
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/security/auth/AuthDetailService.java
@@ -0,0 +1,22 @@
+package com.submit.toyproject.rms_backend_springboot.security.auth;
+
+import com.submit.toyproject.rms_backend_springboot.domain.user.UserRepository;
+import com.submit.toyproject.rms_backend_springboot.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
