d7a6ac7efab38c0447afcfcc5f984b5923adb0e6
kwakdony <kwakdh25@gmail.com>
2021-09-09 13:31:16 +0900

    [UPDATE] SecurityConfiguration

--- src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
@@ -1,18 +1,33 @@
 package com.submit.toyproject.rms_backend_springboot.config;
 
+import com.submit.toyproject.rms_backend_springboot.security.jwt.JwtAuthenticationFilter;
+import com.submit.toyproject.rms_backend_springboot.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable();
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers ( "/auth/**" ).permitAll ()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore (new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        http
+                .headers().frameOptions().disable();
     }
 
     @Override
