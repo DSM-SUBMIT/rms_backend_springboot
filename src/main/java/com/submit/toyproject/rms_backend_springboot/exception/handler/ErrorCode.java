df6244f36cff20cbc98b37b1cb1131ae529f12dc
kwakdony <kwakdh25@gmail.com>
2021-09-10 16:42:42 +0900

    [ADD] Exception

--- src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ErrorCode.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ErrorCode.java
@@ -10,8 +10,9 @@ public enum ErrorCode {
     INVALID_ADMIN_TOKEN(401,"Invalid token."),
     TEAM_ALREADY_EXISTS(409, "Team name is already exists."),
     LANGUAGE_ALREADY_EXISTS(409, "Language is already exists."),
-    USER_NOT_FOUND(404, "User not found"),
-    INVALID_EMAIL(400, "Google email must end with @dsm.hs.kr");
+    USER_NOT_FOUND(404, "User not found."),
+    INVALID_EMAIL(400, "Google email must end with '@dsm.hs.kr'."),
+    INVALID_ID_TOKEN_INFORMATION(400, "Id_Token information does not match.");
 
     private final int status;
     private final String message;
