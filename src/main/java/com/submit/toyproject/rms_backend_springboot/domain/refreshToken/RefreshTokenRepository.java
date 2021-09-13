ef3d888d480a9847769ac290b4a7de8b52b0f5c2
kwakdony <kwakdh25@gmail.com>
2021-09-13 23:45:29 +0900

    [ADD] RefreshToken

---
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/domain/refreshToken/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.submit.toyproject.rms_backend_springboot.domain.refreshToken;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
