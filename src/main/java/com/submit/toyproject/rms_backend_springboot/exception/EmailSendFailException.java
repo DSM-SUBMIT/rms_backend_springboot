7613cdd682fd3395eaa5727d351363ed2403d233
kwakdony <kwakdh25@gmail.com>
2021-09-18 21:25:23 +0900

    [ADD] exception
    실수

--- src/main/java/com/submit/toyproject/rms_backend_springboot/exception/EmailSendFailException.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/exception/EmailSendFailException.java
@@ -1,2 +1,10 @@
-package com.submit.toyproject.rms_backend_springboot.exception;public class EmailSendFailException {
+package com.submit.toyproject.rms_backend_springboot.exception;
+
+import com.submit.toyproject.rms_backend_springboot.exception.handler.ErrorCode;
+import com.submit.toyproject.rms_backend_springboot.exception.handler.RmsException;
+
+public class EmailSendFailException extends RmsException {
+    public EmailSendFailException() {
+        super(ErrorCode.EMAIL_SEND_FAIL);
+    }
 }
