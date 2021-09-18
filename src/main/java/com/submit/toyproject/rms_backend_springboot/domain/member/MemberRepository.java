b03fc65270e23f524e72f44d932376f9001e021c
이승윤 <yyuunn17@naver.com>
2021-09-18 12:59:17 +0900

    [ADD] JPA Method

--- src/main/java/com/submit/toyproject/rms_backend_springboot/domain/member/MemberRepository.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/domain/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.submit.toyproject.rms_backend_springboot.domain.member;
 
+import com.submit.toyproject.rms_backend_springboot.domain.project.Project;
+import com.submit.toyproject.rms_backend_springboot.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MemberRepository extends CrudRepository<Member, MemberId> {
+    Boolean existsByProjectAndUser(Project project, User user);
 }
