2e8148970222e75708f3b4900522019784bd4b17
이승윤 <yyuunn17@naver.com>
2021-09-20 13:25:43 +0900

    [REFACTOR] 계획서 관련 기능 리팩토링

--- src/main/java/com/submit/toyproject/rms_backend_springboot/controller/PlanController.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/controller/PlanController.java
@@ -4,6 +4,8 @@ import com.submit.toyproject.rms_backend_springboot.dto.request.PlanRequest;
 import com.submit.toyproject.rms_backend_springboot.dto.response.PlanResponse;
 import com.submit.toyproject.rms_backend_springboot.service.plan.PlanService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +19,13 @@ public class PlanController {
 
     private final PlanService planService;
 
-    @ApiOperation(value = "계획서 저장", notes = "작성한 계획서를 임시저장하는 기능입니다.")
+    @ApiOperation(value = "계획서 저장")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "계획서 저장/수정이 성공적으로 이루어짐."),
+            @ApiResponse(code = 400, message = "계획서가 이미 제출됨."),
+            @ApiResponse(code = 401, message = "프로젝트 생성자가 아님."),
+            @ApiResponse(code = 404, message = "프로젝트/계획서를 찾을 수 없음.")
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{id}")
     public void savePlan(@PathVariable Integer id,
@@ -25,21 +33,24 @@ public class PlanController {
         planService.savePlan(id, request);
     }
 
-    @ApiOperation(value = "계획서 수정", notes = "계획서 제출 전 또는 미승인되어 반환되었을 때 계획서를 수정할 수 있습니다.")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("/{id}")
-    public void updatePlan(@PathVariable Integer id,
-                           @RequestBody @Valid PlanRequest request) {
-        planService.updatePlan(id, request);
-    }
-
-    @ApiOperation(value = "계획서 제출", notes = "저장된 계획서를 제출하는 기능입니다. 작성 중 제출할 때는 저장 api를 호출한 이후 제출 api를 호출해야 합니다.")
+    @ApiOperation(value = "계획서 제출", notes = "계획서 제출 (작성 중 제출할 때는 저장 api를 호출한 이후 제출 api를 호출해야 함)")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "계획서가 성공적으로 제출됨."),
+            @ApiResponse(code = 400, message = "계획서가 이미 제출됨."),
+            @ApiResponse(code = 401, message = "프로젝트 생성자가 아님."),
+            @ApiResponse(code = 404, message = "계획서를 찾을 수 없음.")
+    })
     @PostMapping("/submit/{id}")
     public void submitPlan(@PathVariable Integer id) {
         planService.submitPlan(id);
     }
 
     @ApiOperation(value = "계획서 보기", notes = "승인된 계획서가 아니라면 멤버만 확인할 수 있습니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "계획서를 성공적으로 가져옴"),
+            @ApiResponse(code = 403, message = "계획서 보기에 대한 권한이 없음."),
+            @ApiResponse(code = 404, message = "계획서를 찾을 수 없음.")
+    })
     @GetMapping("/{id}")
     public PlanResponse getPlanInfo(@PathVariable Integer id) {
         return planService.getPlanInfo(id);
--- src/main/java/com/submit/toyproject/rms_backend_springboot/service/plan/PlanService.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/service/plan/PlanService.java
@@ -5,7 +5,6 @@ import com.submit.toyproject.rms_backend_springboot.dto.response.PlanResponse;
 
 public interface PlanService {
     void savePlan(Integer projectId, PlanRequest request);
-    void updatePlan(Integer id, PlanRequest request);
     void submitPlan(Integer id);
     PlanResponse getPlanInfo(Integer id);
 }
