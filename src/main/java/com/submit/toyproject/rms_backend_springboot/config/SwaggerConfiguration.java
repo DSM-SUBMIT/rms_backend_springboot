cb149ad4f9815275ab7c8cef876843acb86655f3
kwakdony <kwakdh25@gmail.com>
2021-09-21 14:47:49 +0900

    [FIX] swagger 볼 때 IlligalStateException 해결

--- src/main/java/com/submit/toyproject/rms_backend_springboot/config/SwaggerConfiguration.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SwaggerConfiguration.java
@@ -2,6 +2,8 @@ package com.submit.toyproject.rms_backend_springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -13,6 +15,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @EnableSwagger2
@@ -23,6 +26,11 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add( new PageableHandlerMethodArgumentResolver());
     }
 
     @Bean
