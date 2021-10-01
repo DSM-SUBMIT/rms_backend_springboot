41b2a2136cc8a7b760afc91f6dd91ca37c586fce
kwakdony <kwakdh25@gmail.com>
2021-10-01 23:38:58 +0900

    [UPDATE]

--- src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.submit.toyproject.rms_backend_springboot.security.jwt.JwtTokenProvide
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
