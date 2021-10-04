983441016d93bf011f73981acfb4e5c503d5fd06
kwakdony <kwakdh25@gmail.com>
2021-10-04 15:40:27 +0900

    정리

--- src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
@@ -3,22 +3,13 @@ package com.submit.toyproject.rms_backend_springboot.config;
 import com.submit.toyproject.rms_backend_springboot.exception.handler.ExceptionHandlerFilter;
 import com.submit.toyproject.rms_backend_springboot.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.http.HttpServletRequest;
 
 @EnableWebSecurity
 @Configuration
