7e9c8c8dcc598f0acfd86440ed8e5b68203fb418
이승윤 <yyuunn17@naver.com>
2021-10-05 19:29:23 +0900

    [UPDATE]

--- src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ExceptionHandlerFilter.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ExceptionHandlerFilter.java
@@ -4,18 +4,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
             try {
                 chain.doFilter(request, response);
-            } catch(Exception e) {
-                e.printStackTrace();
+            } catch(RmsException e) {
+                ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+                response.setContentType("application/json");
+                response.setStatus(errorResponse.getStatus());
+                response.getWriter().write(errorResponse.toString());
             }
     }
 
