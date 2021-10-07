0380209def514e2680c650fb86c09bc02e2b123c
이승윤 <yyuunn17@naver.com>
2021-10-07 13:45:41 +0900

    [DELETE] ExceptionHanlderFilter

--- src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.submit.toyproject.rms_backend_springboot.config;
 
-import com.submit.toyproject.rms_backend_springboot.exception.handler.ExceptionHandlerFilter;
 import com.submit.toyproject.rms_backend_springboot.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +16,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final ExceptionHandlerFilter exceptionHandlerFilter;
     private final CorsFilter corsFilter;
 
     @Override
@@ -32,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
                     .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
-                .apply(new FilterConfiguration(jwtTokenProvider, exceptionHandlerFilter, corsFilter));
+                .apply(new FilterConfiguration(jwtTokenProvider, corsFilter));
         http
                 .headers().frameOptions().disable();
     }
