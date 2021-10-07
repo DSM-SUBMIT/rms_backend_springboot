7e8587df22c8f8804910f54f847ef80040327046
이승윤 <yyuunn17@naver.com>
2021-10-07 18:50:09 +0900

    [UPDATE]

--- src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ErrorResponse.java
+++ src/main/java/com/submit/toyproject/rms_backend_springboot/exception/handler/ErrorResponse.java
@@ -10,4 +10,12 @@ public class ErrorResponse {
     private final int status;
     private final String message;
 
+    @Override
+    public String toString() {
+        return "{\n" +
+                "\t\"status\": " + status +
+                ",\n\t\"message\": \"" + message + '\"' +
+                "\n}";
+    }
+
 }
